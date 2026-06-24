15a14042cd180902f888cbc4f02a4ace86c91c98
juliocesarsantosrochasilva-dot <juliocesarsantosrochasilva@gmail.com>
2026-06-23 23:48:49 -0300

    Create Questao-06.py

---
+++ Questao-06.py
@@ -0,0 +1,16 @@
+Jornalista = input("Informe o sobrenome do apresentador: ")
+
+if Jornalista == "Bonner":
+    print("Jornal brasileiro")
+
+elif Jornalista == "Vasconcelos":
+    print("Jornal brasileiro")
+
+elif Jornalista == "Pinheiro":
+    print("Bom Dia Nação")
+
+elif Jornalista == "Araujo":
+    print("Bom dia Nação")
+
+else:
+    print("Apresentador(a) desconhecido(a).")
